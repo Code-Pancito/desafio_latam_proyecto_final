b88e6ee1b1802ea7e23da3d9d226c9bd901c6d9f
Code Pancito <codepancito@gmail.com>
2020-09-17 11:54:56 -0300

    - First Commit

---
+++ app/src/main/java/com/codepancito/yu_gi_ohmonsterbattle/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.codepancito.yu_gi_ohmonsterbattle.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.codepancito.yu_gi_ohmonsterbattle.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
